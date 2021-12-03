9054fc8c51a41326eb8439eba465a2bf683f5b2c
Team LevX <team@levx.io>
2021-12-03 22:35:43 +0900

    Update ERC20Airdrops

--- contracts/ERC20Airdrops.sol
+++ contracts/ERC20Airdrops.sol
@@ -9,10 +9,11 @@ import "./MerkleProof.sol";
 contract ERC20Airdrops is MerkleProof {
     using SafeERC20 for IERC20;
 
+    mapping(address => mapping(bytes32 => uint256)) public deadlineOf;
     mapping(address => mapping(bytes32 => address)) public walletOf;
     mapping(address => mapping(bytes32 => mapping(bytes32 => bool))) internal _hasClaimed;
 
-    event AddMerkleRoot(address indexed token, bytes32 indexed merkleRoot, address wallet);
+    event AddMerkleRoot(address indexed token, bytes32 indexed merkleRoot, address wallet, uint256 deadline);
     event Claim(
         address indexed token,
         bytes32 indexed merkleRoot,
@@ -21,12 +22,17 @@ contract ERC20Airdrops is MerkleProof {
         uint256 amount
     );
 
-    function addMerkleRoot(address token, bytes32 merkleRoot) external {
+    function addMerkleRoot(
+        address token,
+        bytes32 merkleRoot,
+        uint256 deadline
+    ) external {
         address wallet = walletOf[token][merkleRoot];
         require(wallet == address(0), "LEVX: DUPLICATE_ROOT");
         walletOf[token][merkleRoot] = msg.sender;
+        deadlineOf[token][merkleRoot] = deadline;
 
-        emit AddMerkleRoot(token, merkleRoot, msg.sender);
+        emit AddMerkleRoot(token, merkleRoot, msg.sender, deadline);
     }
 
     function claim(
@@ -37,6 +43,7 @@ contract ERC20Airdrops is MerkleProof {
     ) external {
         address wallet = walletOf[token][merkleRoot];
         require(wallet != address(0), "LEVX: INVALID_ROOT");
+        require(deadlineOf[token][merkleRoot] < block.timestamp, "LEVX: EXPIRED");
 
         bytes32 leaf = keccak256(abi.encodePacked(msg.sender, amount));
         require(!_hasClaimed[token][merkleRoot][leaf], "LEVX: FORBIDDEN");
