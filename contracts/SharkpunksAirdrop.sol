9f24b454190664816226271bc1dac3d356c28c1d
LevX <team@levx.io>
2022-03-30 10:47:11 +0400

    Update SharkpunksAirdrop

--- contracts/SharkpunksAirdrop.sol
+++ contracts/SharkpunksAirdrop.sol
@@ -2,58 +2,72 @@
 
 pragma solidity =0.8.3;
 
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import "@shoyunft/contracts/contracts/interfaces/INFT721.sol";
 
-contract SharkpunksAirdrop {
+contract SharkpunksAirdrop is Ownable {
     address public immutable sharkpunks;
-    address public immutable signer;
-    uint64 public immutable deadline;
-    uint256 internal _tokenId;
+    mapping(bytes32 => Airdrop) public airdrops;
     mapping(bytes32 => mapping(bytes32 => bool)) _minted;
+    uint256 internal _tokenId;
 
-    event Claim(bytes32 indexed id, bytes32 indexed memo, address indexed to, uint256 tokenId);
+    struct Airdrop {
+        address signer;
+        uint32 deadline;
+        uint32 max;
+        uint32 minted;
+    }
 
-    constructor(
-        address _sharkpunks,
-        address _signer,
-        uint64 _deadline,
-        uint256 fromTokenId
-    ) {
+    event Add(bytes32 indexed slug, address signer, uint32 deadline, uint32 max);
+    event Claim(bytes32 indexed slug, bytes32 indexed id, address indexed to, uint256 tokenId);
+
+    constructor(address _sharkpunks, uint256 fromTokenId) {
         sharkpunks = _sharkpunks;
-        signer = _signer;
-        deadline = _deadline;
         _tokenId = fromTokenId;
     }
 
-    function mintMultiple(address[] calldata recipients) external {
-        require(msg.sender == signer, "LEVX: FORIBDDEN");
+    function add(
+        bytes32 slug,
+        address signer,
+        uint32 deadline,
+        uint32 max
+    ) external onlyOwner {
+        Airdrop storage airdrop = airdrops[slug];
+        require(airdrop.signer == address(0), "LEVX: ADDED");
 
-        for (uint256 i; i < recipients.length; i++) {
-            INFT721(sharkpunks).mint(recipients[i], _tokenId++, "");
-        }
+        airdrop.signer = signer;
+        airdrop.deadline = deadline;
+        airdrop.max = max;
+
+        emit Add(slug, signer, deadline, max);
     }
 
     function claim(
+        bytes32 slug,
         bytes32 id,
-        bytes32 memo,
         address to,
         bytes calldata data,
         uint8 v,
         bytes32 r,
         bytes32 s
     ) external {
-        uint64 _now = uint64(block.timestamp);
-        require(_now < deadline, "LEVX: EXPIRED");
-        require(!_minted[id][memo], "LEVX: MINTED");
+        Airdrop storage airdrop = airdrops[slug];
+        address signer = airdrop.signer;
 
-        bytes32 message = keccak256(abi.encodePacked(id, memo));
+        require(signer != address(0), "LEVX: INVALID_SLUG");
+        require(airdrop.max == 0 || airdrop.minted < airdrop.max, "LEVX: FINISHED");
+        require(uint32(block.timestamp) < airdrop.deadline, "LEVX: EXPIRED");
+        require(!_minted[slug][id], "LEVX: MINTED");
+
+        bytes32 message = keccak256(abi.encodePacked(slug, id));
         require(ECDSA.recover(ECDSA.toEthSignedMessageHash(message), v, r, s) == signer, "LEVX: UNAUTHORIZED");
 
-        _minted[id][memo] = true;
+        airdrop.minted++;
+        _minted[slug][id] = true;
 
         uint256 tokenId = _tokenId++;
-        emit Claim(id, memo, to, tokenId);
+        emit Claim(slug, id, to, tokenId);
         INFT721(sharkpunks).mint(to, tokenId, data);
     }
 }
