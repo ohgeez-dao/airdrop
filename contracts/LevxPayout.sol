c36959103c2b310c1db47840a0f064715e22b26a
LevX <team@levx.io>
2022-04-05 19:57:15 +0900

    Fix LevxPayout

--- contracts/LevxPayout.sol
+++ contracts/LevxPayout.sol
@@ -13,9 +13,9 @@ contract LevxPayout is Ownable {
 
     address public immutable levx;
 
-    Stream[] public streams;
+    Payout[] public payouts;
 
-    struct Stream {
+    struct Payout {
         address wallet;
         address recipient;
         uint32 startedAt;
@@ -41,12 +41,13 @@ contract LevxPayout is Ownable {
     ) external onlyOwner {
         require(amount > 0, "LEVX: INVALID_AMOUNT");
 
-        uint256 id = streams.length;
-        Stream storage stream = streams.push();
-        stream.wallet = wallet;
-        stream.recipient = recipient;
-        stream.startedAt = uint32(block.timestamp);
-        stream.amount = amount;
+        uint256 id = payouts.length;
+        Payout storage payout = payouts.push();
+        payout.wallet = wallet;
+        payout.recipient = recipient;
+        payout.startedAt = uint32(block.timestamp);
+        payout.duration = duration;
+        payout.amount = amount;
 
         emit Start(id, wallet, recipient, duration, amount);
 
@@ -54,21 +55,21 @@ contract LevxPayout is Ownable {
     }
 
     function stop(uint256 id) external onlyOwner {
-        Stream storage stream = streams[id];
-        require(stream.stoppedAt == 0, "LEVX: STOPPED");
+        Payout storage payout = payouts[id];
+        require(payout.stoppedAt == 0, "LEVX: STOPPED");
 
-        uint256 released = _amountReleased(stream);
-        uint256 remaining = stream.amount - released;
+        uint256 released = _amountReleased(payout);
+        uint256 remaining = payout.amount - released;
         require(remaining > 0, "LEVX: FINISHED");
 
-        uint256 pending = released - stream.claimed;
-        stream.claimed = released;
-        stream.stoppedAt = uint32(block.timestamp);
+        payout.claimed = released;
+        payout.stoppedAt = uint32(block.timestamp);
 
+        uint256 pending = released - payout.claimed;
         emit Stop(id, pending);
-        IERC20(levx).safeTransfer(stream.wallet, remaining);
+        IERC20(levx).safeTransfer(payout.wallet, remaining);
         if (pending > 0) {
-            IERC20(levx).safeTransfer(stream.recipient, pending);
+            IERC20(levx).safeTransfer(payout.recipient, pending);
         }
     }
 
@@ -77,12 +78,12 @@ contract LevxPayout is Ownable {
         address to,
         bytes calldata callData
     ) external {
-        Stream storage stream = streams[id];
-        require(stream.recipient == msg.sender, "LEVX: FORBIDDEN");
+        Payout storage payout = payouts[id];
+        require(payout.recipient == msg.sender, "LEVX: FORBIDDEN");
 
-        uint256 released = _amountReleased(stream);
-        uint256 pending = released - stream.claimed;
-        stream.claimed = released;
+        uint256 released = _amountReleased(payout);
+        uint256 pending = released - payout.claimed;
+        payout.claimed = released;
 
         if (to == address(0)) {
             emit Claim(id, pending, msg.sender);
@@ -97,11 +98,11 @@ contract LevxPayout is Ownable {
     }
 
     function pendingAmount(uint256 id) external view returns (uint256) {
-        Stream storage stream = streams[id];
-        return _amountReleased(stream) - stream.claimed;
+        Payout storage payout = payouts[id];
+        return _amountReleased(payout) - payout.claimed;
     }
 
-    function _amountReleased(Stream storage stream) internal view returns (uint256) {
+    function _amountReleased(Payout storage stream) internal view returns (uint256) {
         uint256 duration = block.timestamp - stream.startedAt;
         uint256 _total = uint256(stream.duration);
         if (duration >= _total) return stream.amount;
