c7022329fd27d6cff26f6c9c535c1ba67338e3df
LevX <team@levx.io>
2022-04-06 04:23:24 +0900

    Deploy LevxPayout to mainnet

--- deploy/04_LevPayout.ts
+++ deploy/04_LevPayout.ts
@@ -2,12 +2,11 @@ export default async ({ getNamedAccounts, deployments }) => {
     const { deploy } = deployments;
     const { deployer } = await getNamedAccounts();
 
-    const owner = "0x0903f8892c06A99bf1D68088fAB597a0762e0BC8";
     const levx = "0xf474E526ADe9aD2CC2B66ffCE528B1A51B91FCdC";
 
-    await deploy("LevxAirdrop", {
+    await deploy("LevxPayout", {
         from: deployer,
-        args: [owner, levx],
+        args: [levx],
         log: true,
     });
 };
